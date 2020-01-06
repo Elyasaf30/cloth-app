fa54336e42aa9db418d47bd79e1517967e1031d1
Elyasaf Hershkovitz <elyasaf_h@publicismedia.co.il>
2020-01-06 17:32:14 +0200

    added directory state into reduxReducer, Next need to implement the collection state into reactReducer

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,63 +1,23 @@
 import React from "react";
 
 import MenuItem from "../menu-item/menu-item.component";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { selectDirectorySections } from "../../redux/directory/directory.selectors";
 
 import "./directory.style.scss";
 
-class Directory extends React.Component {
-  constructor() {
-    super();
+const Directory = ({ sections }) => (
+  <div className="directory-menu">
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
+);
 
-    this.state = {
-      sections: [
-        {
-          title: "Hats",
-          imageUrl:
-            "https://images.unsplash.com/photo-1533055640609-24b498dfd74c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-          id: 1,
-          linkUrl: "shop/hats"
-        },
-        {
-          title: "Jackets",
-          imageUrl:
-            "https://images.unsplash.com/photo-1489286696299-aa7486820bd5?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-          id: 2,
-          linkUrl: "shop/jackets"
-        },
-        {
-          title: "Sneakers",
-          imageUrl:
-            "https://images.unsplash.com/photo-1529810313688-44ea1c2d81d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-          id: 3,
-          linkUrl: "shop/sneakers"
-        },
-        {
-          title: "Womens",
-          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-          size: "large",
-          id: 4,
-          linkUrl: "shop/womens"
-        },
-        {
-          title: "Mens",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-          size: "large",
-          id: 5,
-          linkUrl: "shop/mens"
-        }
-      ]
-    };
-  }
+const mapToStateProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
 
-  render() {
-    return (
-      <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default Directory;
+export default connect(mapToStateProps)(Directory);
