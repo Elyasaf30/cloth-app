53502e28938fde8092285ffd96366bb2aa4c1a5b
elyasaf Hershkovitz <Elyuk1988@gmail.com>
2020-05-13 14:36:30 +0300

    update app,signIn,SignUp and shop components with hooks--->useState & UseEffect

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 
@@ -7,28 +7,25 @@ import collectionPageContainer from "../collection/collection.container";
 
 import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
 
-class ShopPage extends React.Component {
-  componentDidMount() {
-    const { fetchCollectionsStart } = this.props;
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+  useEffect(() => {
     fetchCollectionsStart();
-  }
-  render() {
-    const { match } = this.props;
-    return (
-      <div className="shop-page">
-        <Route
-          exact
-          path={`${match.path}`}
-          component={collectionOverviewContainer}
-        />
-        <Route
-          path={`${match.path}/:collectionId`}
-          component={collectionPageContainer}
-        />
-      </div>
-    );
-  }
-}
+  }, [fetchCollectionsStart]);
+
+  return (
+    <div className="shop-page">
+      <Route
+        exact
+        path={`${match.path}`}
+        component={collectionOverviewContainer}
+      />
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={collectionPageContainer}
+      />
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
   fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
